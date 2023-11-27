b25613493091cde310ea8a181421fdf63d005bd8
Maria Edström <mariat.edstrom@gmail.com>
2023-11-27 14:13:04 +0100

    Add project files.

---
+++ EntityFramework/Entities/Product.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EntityFramework.Entities;
+
+public class Product
+{
+    [Key]
+    public int ID { get; set; }
+    [Required]
+    [MaxLength(50)]
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public double Price { get; set; }
+}
